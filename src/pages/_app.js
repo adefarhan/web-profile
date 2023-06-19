4fe58b82f3c919fca21310a6a4dd54c49ef33b57
adefarhan <adefarhanzikri@gmail.com>
2023-06-19 18:00:43 +0700

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
+import {Montserrat} from "next/font/google"
+import Head from 'next/head'
+
+const monsterrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+  display: 'swap'
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={`${monsterrat.variable} bg-light w-full min-h-screen` }>
+      <NavBar/>
+      <Component {...pageProps} />
+    </main>
+    </>
+  )
 }
