1f05713446911e9b462dfb8f699b9ebd88f99c71
Ade Farhan <adefarhanzikri@gmail.com>
2023-06-28 00:15:20 +0700

    lilcon v2

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,10 +3,14 @@ import { motion, useScroll } from "framer-motion";
 import LiIcon from "./LiIcon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef();
   return (
-    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
-      <LiIcon />
-      <div>
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between"
+    >
+      <LiIcon reference={ref} />
+      <motion.div>
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
           <a
@@ -21,7 +25,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
           {time} | {address}
         </span>
         <p className="font-medium w-full">{work}</p>
-      </div>
+      </motion.div>
     </li>
   );
 };
@@ -39,7 +43,7 @@ const Experience = () => {
       </h2>
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
-          className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
           style={{ scaleY: scrollYProgress }}
         />
 
